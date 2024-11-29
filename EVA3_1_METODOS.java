ad3a55578754343a76cc3a4dfcda92ec535576f0
gerasantillan17 <gerapumas122@gmail.com>
2024-11-29 16:55:56 -0600

    Add files via upload

---
+++ EVA3_1_METODOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+/**
+ *
+ * @author gerar
+ */
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        imprimirMensaje("Hola MUndo");
+    }
+    //Declaracion del codigo
+    public static void imprimirMensaje(String mensaje){
+        //Cuerpo del metodo
+      System.out.println("Tu mensaje:" + mensaje);  
+    }
+    
+}
